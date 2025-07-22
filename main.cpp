e200c8c69c747333d039c1b2adbb9359cf099c4f
Joseph Middleton <j.eg.middleton5@gmail.com>
2025-07-22 17:41:02 +0100

    Reset upon opening bash.

--- main.cpp
+++ main.cpp
@@ -572,7 +572,7 @@ int main() {
     };
 
     const auto get_test_button = [&]{
-        return ftxui::Button("Launch Local Shell", [&]{bool_launch_system_execution = true; launch_system_execution_path = "/bin/bash"; }, ftxui::ButtonOption::Ascii());
+        return ftxui::Button("Launch Local Shell", [&]{bool_launch_system_execution = true; launch_system_execution_path = "/bin/bash -c reset"; }, ftxui::ButtonOption::Ascii());
     };
 
     const auto get_back_button = [&]{
